a965e85155c3b74f9d34c6b5965816df3225a568
cezarmathe <cezarmathe@gmail.com>
2019-06-12 19:50:47 +0300

    Add JSON and tests

--- src/test/java/CodeDbJsonTest.java
+++ src/test/java/CodeDbJsonTest.java
@@ -1,4 +1,36 @@
-package PACKAGE_NAME;
+import com.hcodez.codeengine.model.CodeDb;
+import org.junit.Test;
+
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
 
 public class CodeDbJsonTest {
+
+    @Test
+    public void jsonToCode() {
+
+        ClassLoader classloader = Thread.currentThread().getContextClassLoader();
+        InputStream inputStream = classloader.getResourceAsStream("json/code_db.json");
+
+        assert inputStream != null;
+
+        CodeDb codeDb = CodeDb.fromJson(new InputStreamReader(inputStream, StandardCharsets.UTF_8));
+        System.out.println("Code: " + codeDb);
+        System.out.println("Code ID: " + codeDb.getId());
+        System.out.println("Code owner ID: " + codeDb.getOwnerId());
+        // FIXME: 2019-06-12 code not loading timestamps from json
+//        System.out.println("Created at: " + codeDb.getCreateTime().toString());
+//        System.out.println("Edited at: " + codeDb.getEditTime().toString());
+        System.out.println("API URL: " + codeDb.getUrl().toString());
+    }
+
+    @Test
+    public void codeToJson() {
+        CodeDb code = new CodeDb();
+        code.setIdentifier("aB12");
+        code.setOwner("cezarmathe");
+        code.setPublicStatus(true);
+        System.out.println(code.toJson());
+    }
 }
--- src/test/java/CodeJsonTest.java
+++ src/test/java/CodeJsonTest.java
@@ -1,4 +1,55 @@
-package PACKAGE_NAME;
+import com.hcodez.codeengine.model.Code;
+import org.joda.time.Instant;
+import org.junit.Test;
+
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
 
 public class CodeJsonTest {
+
+    @Test
+    public void jsonToCode() throws MalformedURLException {
+
+        ClassLoader classloader = Thread.currentThread().getContextClassLoader();
+        InputStream inputStream = classloader.getResourceAsStream("json/code.json");
+
+        assert inputStream != null;
+
+        Code readCode = Code.fromJson(new InputStreamReader(inputStream, StandardCharsets.UTF_8));
+        System.out.println("Code: " + readCode);
+        // FIXME: 2019-06-12 code not loading timestamps from json
+        System.out.println("Created at: " + readCode.getCreateTime());
+        System.out.println("Edited at: " + readCode.getEditTime());
+        System.out.println("API URL: " + readCode.getUrl().toString());
+
+        Code goodCode = new Code();
+        goodCode.setIdentifier("aB12");
+        goodCode.setOwner("cezarmathe");
+        goodCode.setPasscode("d723y7x28");
+        goodCode.setName("A test code");
+        goodCode.setPublicStatus(true);
+        goodCode.setUrl(new URL("https://api.example.com/v0/code/aB12@cezarmathe"));
+        goodCode.setCreateTime(new Instant(1560354133));
+        goodCode.setEditTime(new Instant(1560357733));
+
+//        assert readCode.equals(goodCode);
+    }
+
+    @Test
+    public void codeToJson() throws MalformedURLException {
+        Code code = new Code();
+        code.setIdentifier("aB12");
+        code.setOwner("cezarmathe");
+        code.setPasscode("d723y7x28");
+        code.setName("A test code");
+        code.setPublicStatus(true);
+        code.setUrl(new URL("https://api.example.com/v0/code/aB12@cezarmathe"));
+        code.setCreateTime(new Instant(1560354133));
+        code.setEditTime(new Instant(1560357733));
+
+        System.out.println(code.toJson());
+    }
 }
