23ea157da58a830ed6d902b61ea7e312a13afff4
cezarmathe <cezarmathe@gmail.com>
2019-06-19 14:40:41 +0300

    Fix code parsing tests

--- src/test/java/CodeParserTest.java
+++ src/test/java/CodeParserTest.java
@@ -1,6 +1,7 @@
+import assertions.CodeAssert;
+import com.hcodez.codeengine.model.Code;
 import com.hcodez.codeengine.parser.CodeParser;
 import com.hcodez.codeengine.parser.CodeType;
-import com.hcodez.codeengine.model.Code;
 import org.junit.Test;
 
 import java.io.IOException;
@@ -9,11 +10,11 @@ import java.util.ArrayList;
 public class CodeParserTest {
 
     @Test
-    public void codeFactoryAllCodeExtractionTest() throws IOException {
+    public void codeParserParseStringTest() throws IOException {
 
         CodeParser codeParser = new CodeParser();
 
-        ArrayList<Code> list = codeParser
+        ArrayList<Code> parsedList = codeParser
                 .addCodeType(CodeType.PRIVATE)
                 .addCodeType(CodeType.PUBLIC_NO_PASSCODE)
                 .addCodeType(CodeType.PUBLIC_WITH_PASSCODE)
@@ -21,6 +22,38 @@ public class CodeParserTest {
                         TestCommon.getResourceAsString("plain_text/code_factory_code_list.txt")
                 );
 
-        System.out.println(list);
+        ArrayList<Code> goodList = new ArrayList<>();
+
+        Code code = new Code();
+        code.setIdentifier("1111");
+        code.setOwner("adasdasdas");
+        code.setPasscode("823ijkdw");
+        code.setPublicStatus(true);
+
+        goodList.add(code);
+
+        code = new Code();
+        code.setIdentifier("ab12");
+
+        goodList.add(code);
+
+        code = new Code();
+        code.setIdentifier("123B");
+        code.setOwner("cezarmathe");
+        code.setPublicStatus(true);
+
+        goodList.add(code);
+
+        code = new Code();
+        code.setIdentifier("1111");
+        code.setOwner("numelemeu");
+        code.setPasscode("qudadjas22");
+        code.setPublicStatus(true);
+
+        goodList.add(code);
+
+        for (int i = 0; i < parsedList.size(); i++) {
+            CodeAssert.assertThat(parsedList.get(i)).isEqualTo(goodList.get(i));
+        }
     }
 }
