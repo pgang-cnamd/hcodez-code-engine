683b6f4ac1b2171cd7726b178e6fd530ed69d3a4
cezarmathe <cezarmathe@gmail.com>
2019-06-21 10:34:55 +0300

    Add boilerplate code for faulty CodeTypes

--- src/main/java/com/hcodez/codeengine/model/CodeType.java
+++ src/main/java/com/hcodez/codeengine/model/CodeType.java
@@ -12,12 +12,25 @@ public enum CodeType {
      */
     PRIVATE,
     PUBLIC_NO_PASSCODE,
-    PUBLIC_WITH_PASSCODE;
+    PUBLIC_WITH_PASSCODE,
 
     /**
      * Faulty CodeTypes for detecting faulty Codes
      */
-    // TODO: 2019-06-21 add those CodeTypes, along with their RegEx representation
+    /*private codes*/
+    ERR_PRIVATE_MISS_START_BRACKET,
+    ERR_PRIVATE_MISS_END_BRACKET,
+
+    /*public codes, no passcode*/
+    ERR_PUBLIC_NO_PASSCODE_MISS_START_BRACKET,
+    ERR_PUBLIC_NO_PASSCODE_MISS_AT_SIGN,
+    ERR_PUBLIC_NO_PASSCODE_MISS_END_BRACKET,
+
+    /*public codes with passcode*/
+    ERR_PUBLIC_WITH_PASSCODE_MISS_START_BRACKET,
+    ERR_PUBLIC_WITH_PASSCODE_MISS_AT_SIGN,
+    ERR_PUBLIC_WITH_PASSCODE_MISS_EXCLAM_SIGN,
+    ERR_PUBLIC_WITH_PASSCODE_MISS_END_BRACKET;
 
     /**
      * Get the regular expression for the given code type
@@ -25,12 +38,35 @@ public enum CodeType {
      */
     public String getRegex() {
         switch (this) {
+            /*proper CodeType regex*/
             case PRIVATE:
                 return "<(?<identifier>[0-9a-zA-Z]{4})>";
             case PUBLIC_NO_PASSCODE:
                 return "<(?<identifier>[0-9a-zA-Z]{4})@(?<owner>[0-9a-zA-Z]{4,16})>";
             case PUBLIC_WITH_PASSCODE:
                 return "<(?<identifier>[0-9a-zA-Z]{4})@(?<owner>[0-9a-zA-Z]{4,16})!(?<passcode>[0-9a-zA-Z]{4,16})>";
+
+            /*faulty CodeType regex*/ // TODO: 2019-06-21 add regular expressions
+            case ERR_PRIVATE_MISS_START_BRACKET:
+                return "";
+            case ERR_PRIVATE_MISS_END_BRACKET:
+                return "";
+
+            case ERR_PUBLIC_NO_PASSCODE_MISS_START_BRACKET:
+                return "";
+            case ERR_PUBLIC_NO_PASSCODE_MISS_AT_SIGN:
+                return "";
+            case ERR_PUBLIC_NO_PASSCODE_MISS_END_BRACKET:
+                return "";
+                
+            case ERR_PUBLIC_WITH_PASSCODE_MISS_START_BRACKET:
+                return "";
+            case ERR_PUBLIC_WITH_PASSCODE_MISS_END_BRACKET:
+                return "";
+            case ERR_PUBLIC_WITH_PASSCODE_MISS_AT_SIGN:
+                return "";
+            case ERR_PUBLIC_WITH_PASSCODE_MISS_EXCLAM_SIGN:
+                return "";
             default:
                 throw new RuntimeException("unknown code type");
         }
