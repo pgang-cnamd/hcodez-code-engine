ff2a1975afaf8a0e19bf191b2c603ceba72d22c5
cezarmathe <cezarmathe@gmail.com>
2019-06-26 18:56:37 +0300

    Implement CodeType serializer and deserializer

--- src/main/java/com/hcodez/codeengine/json/CodeTypeDeserializer.java
+++ src/main/java/com/hcodez/codeengine/json/CodeTypeDeserializer.java
@@ -1,4 +1,16 @@
 package com.hcodez.codeengine.json;
 
-public class CodeTypeDeserializer {
+import com.google.gson.JsonDeserializationContext;
+import com.google.gson.JsonDeserializer;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParseException;
+import com.hcodez.codeengine.model.CodeType;
+
+import java.lang.reflect.Type;
+
+public class CodeTypeDeserializer implements JsonDeserializer<CodeType> {
+    @Override
+    public CodeType deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context) throws JsonParseException {
+        return CodeType.fromString(json.getAsJsonPrimitive().getAsString());
+    }
 }
--- src/main/java/com/hcodez/codeengine/json/CodeTypeSerializer.java
+++ src/main/java/com/hcodez/codeengine/json/CodeTypeSerializer.java
@@ -1,4 +1,16 @@
 package com.hcodez.codeengine.json;
 
-public class CodeTypeSerializer {
+import com.google.gson.JsonElement;
+import com.google.gson.JsonPrimitive;
+import com.google.gson.JsonSerializationContext;
+import com.google.gson.JsonSerializer;
+import com.hcodez.codeengine.model.CodeType;
+
+import java.lang.reflect.Type;
+
+public class CodeTypeSerializer implements JsonSerializer<CodeType> {
+    @Override
+    public JsonElement serialize(CodeType src, Type typeOfSrc, JsonSerializationContext context) {
+        return new JsonPrimitive(src.toString());
+    }
 }
