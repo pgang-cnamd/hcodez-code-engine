de6b9819c64fc68bae04b3df3446c93cfc635ac5
cezarmathe <cezarmathe@gmail.com>
2019-06-28 18:40:09 +0300

    Add easy one code type parsing in CodeParserTest

--- src/test/java/com/hcodez/parser/CodeParserTest.java
+++ src/test/java/com/hcodez/parser/CodeParserTest.java
@@ -1,14 +1,17 @@
 package com.hcodez.parser;
 
-import com.hcodez.util.CodeAssert;
 import com.hcodez.codeengine.builder.CodeBuilder;
 import com.hcodez.codeengine.model.Code;
-import com.hcodez.codeengine.parser.CodeParser;
 import com.hcodez.codeengine.model.CodeType;
+import com.hcodez.codeengine.parser.CodeParser;
+import com.hcodez.util.CodeAssert;
+import com.hcodez.util.TestCommon;
+import com.hcodez.util.Xml;
 import org.junit.After;
 import org.junit.Test;
-import com.hcodez.util.TestCommon;
+import org.xml.sax.SAXException;
 
+import javax.xml.parsers.ParserConfigurationException;
 import java.io.IOException;
 import java.util.ArrayList;
 
@@ -60,6 +63,23 @@ public class CodeParserTest {
                         .withPublicStatus(true)
                         .build()
         );
+
+        for (int i = 0; i < parsedList.size(); i++) {
+            CodeAssert.assertThat(parsedList.get(i)).isEqualTo(goodList.get(i));
+        }
+    }
+
+    @Test
+    public void parseEasyPrivateCodes() throws IOException, ParserConfigurationException, SAXException {
+        CodeParser parser = new CodeParser();
+
+        ArrayList<Code> parsedList = parser
+                .addCodeType(CodeType.PRIVATE)
+                .parseString(TestCommon.getResourceAsString("/plain_text/codes_private.txt"));
+
+        ArrayList<Code> goodList = Xml.loadCodesFromXml(CodeType.PRIVATE);
+
+        assert parsedList.size() == goodList.size();
 
         for (int i = 0; i < parsedList.size(); i++) {
             CodeAssert.assertThat(parsedList.get(i)).isEqualTo(goodList.get(i));
